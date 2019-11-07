9aa3f35eb3fde8516e9b2deaebd10705c7d37bac
FreakyCoder <kurayogun@gmail.com>
2019-11-07 13:06:10 +0300

    Bounce animation is integrated

--- example/lib/src/components/DuoToggleSwitch/DuoToggleSwitch.js
+++ example/lib/src/components/DuoToggleSwitch/DuoToggleSwitch.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Text, View, Animated } from "react-native";
+import { Text, Easing, Animated } from "react-native";
 import Androw from "react-native-androw";
 import Ripple from "react-native-material-ripple";
 import {
@@ -15,30 +15,23 @@ export default class DuoToggleSwitch extends Component {
     super(props);
     this.state = {
       activeTab: 0,
-      animation: new Animated.Value(0)
+      animation: new Animated.Value(1)
     };
   }
 
-  StartBackgroundColorAnimation = () => {
-    this.state.animation.setValue(0);
-
-    Animated.timing(this.state.animation, {
+  spring = () => {
+    const { animation } = this.state;
+    animation.setValue(0.98);
+    Animated.spring(animation, {
       toValue: 1,
-      duration: 15000
-    }).start(() => {
-      this.StartBackgroundColorAnimation();
-    });
+      friction: 3
+    }).start();
   };
 
   setActiveTabColor = alignment => {
-    this.StartBackgroundColorAnimation();
-    const color = this.state.animation.interpolate({
-      inputRange: [0, 300],
-      outputRange: ["rgba(255, 0, 0, 1)", "rgba(0, 255, 0, 1)"]
-    });
     const { activeColor, inactiveColor } = this.props;
     const { activeTab } = this.state;
-    // return color;
+
     if (!alignment) return activeTab === 0 ? inactiveColor : activeColor;
     return activeTab === 1 ? inactiveColor : activeColor;
   };
@@ -61,13 +54,20 @@ export default class DuoToggleSwitch extends Component {
       activeTextColor,
       inactiveTextColor
     } = this.props;
-    const { activeTab } = this.state;
+    const { activeTab, animation } = this.state;
 
     return (
       <Androw style={shadowStyle || _shadowStyle(shadowColor)}>
-        <Animated.View style={container(height, width, backgroundColor)}>
+        <Animated.View
+          style={[
+            container(height, width, backgroundColor),
+            { transform: [{ scale: animation }] }
+          ]}
+        >
           <Ripple
+            disable
             onPress={() => {
+              this.spring(this, Easing.bounce);
               this.setState({ activeTab: 0 });
               onPress && onPress;
             }}
@@ -93,6 +93,7 @@ export default class DuoToggleSwitch extends Component {
             rippleColor={inactiveColor}
             rippleContainerBorderRadius={50}
             onPress={() => {
+              this.spring(this, Easing.bounce);
               this.setState({ activeTab: 1 });
               onPress && onPress;
             }}
