63ecafa9ed3a4423f4a20f339366ab005bbfadaf
FreakyCoder <kurayogun@gmail.com>
2023-01-06 17:54:57 +0300

    chore: new example is updated

--- example/App.tsx
+++ example/App.tsx
@@ -1,32 +1,29 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import Ripple from "react-native-material-ripple";
-import DuoToggleSwitch from "react-native-duo-toggle-switch";
+import {StyleSheet, View} from 'react-native';
+import Ripple from 'react-native-material-ripple';
+import DuoToggleSwitch from 'react-native-duo-toggle-switch';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
       <View
         style={{
           shadowRadius: 8,
           shadowOpacity: 0.3,
-          shadowColor: "#757575",
+          shadowColor: '#757575',
           shadowOffset: {
             width: 0,
             height: 3,
           },
-        }}
-      >
+        }}>
         <DuoToggleSwitch
           primaryText="Customers"
           secondaryText="Employees"
           onPrimaryPress={() => {}}
           onSecondaryPress={() => {}}
           TouchableComponent={Ripple}
-          primaryButtonStyle={{ width: 150 }}
-          secondaryButtonStyle={{ width: 125 }}
-          primaryTextStyle={{ marginRight: 32 }}
+          primaryButtonStyle={{width: 150}}
+          secondaryButtonStyle={{width: 125}}
+          primaryTextStyle={{marginRight: 32}}
           rippleColor="#fff"
           rippleContainerBorderRadius={50}
         />
@@ -39,7 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 48,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
   },
 });
